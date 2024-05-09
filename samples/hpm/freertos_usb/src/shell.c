5362d680574ba9cfa7dcc0670f8a1729dae24389
sakumisu <1203593632@qq.com>
2024-05-09 19:30:45 +0800

    feat: add freertos with usb demo

--- samples/hpm/freertos_usb/src/shell.c
+++ samples/hpm/freertos_usb/src/shell.c
@@ -4,15 +4,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "board.h"
-#include "hpm_uart_drv.h"
 #include "chry_ringbuffer.h"
 #include "csh.h"
+#include "usbd_core.h"
+#include "usbd_cdc.h"
 
+#ifndef task_repl_PRIORITY
 #define task_repl_PRIORITY (configMAX_PRIORITIES - 4U)
+#endif
+
+#ifndef task_exec_PRIORITY
 #define task_exec_PRIORITY (configMAX_PRIORITIES - 5U)
+#endif
 
 static chry_shell_t csh;
-static UART_Type *shell_uart = NULL;
 static volatile bool login = false;
 static chry_ringbuffer_t shell_rb;
 static uint8_t mempool[1024];
@@ -29,33 +34,182 @@ static TaskHandle_t task_hdl_exec = NULL;
 static EventGroupHandle_t event_hdl;
 static StaticEventGroup_t event_grp;
 
-void shell_uart_isr(void)
+/*!< endpoint address */
+#define CDC_IN_EP  0x81
+#define CDC_OUT_EP 0x01
+#define CDC_INT_EP 0x83
+
+/*!< config descriptor size */
+#define USB_CONFIG_SIZE (9 + CDC_ACM_DESCRIPTOR_LEN)
+
+#ifdef CONFIG_USB_HS
+#define CDC_MAX_MPS 512
+#else
+#define CDC_MAX_MPS 64
+#endif
+
+/*!< global descriptor */
+static const uint8_t cdc_descriptor[] = {
+    USB_DEVICE_DESCRIPTOR_INIT(USB_2_0, 0xEF, 0x02, 0x01, USBD_VID, USBD_PID, 0x0100, 0x01),
+    USB_CONFIG_DESCRIPTOR_INIT(USB_CONFIG_SIZE, 0x02, 0x01, USB_CONFIG_BUS_POWERED, USBD_MAX_POWER),
+    CDC_ACM_DESCRIPTOR_INIT(0x00, CDC_INT_EP, CDC_OUT_EP, CDC_IN_EP, CDC_MAX_MPS, 0x02),
+    /*
+     * string0 descriptor
+     */
+    USB_LANGID_INIT(USBD_LANGID_STRING),
+    /*
+     * string1 descriptor
+     */
+    0x14,                       /* bLength */
+    USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
+    'C', 0x00,                  /* wcChar0 */
+    'h', 0x00,                  /* wcChar1 */
+    'e', 0x00,                  /* wcChar2 */
+    'r', 0x00,                  /* wcChar3 */
+    'r', 0x00,                  /* wcChar4 */
+    'y', 0x00,                  /* wcChar5 */
+    'U', 0x00,                  /* wcChar6 */
+    'S', 0x00,                  /* wcChar7 */
+    'B', 0x00,                  /* wcChar8 */
+    /*
+     * string2 descriptor
+     */
+    0x26,                       /* bLength */
+    USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
+    'C', 0x00,                  /* wcChar0 */
+    'h', 0x00,                  /* wcChar1 */
+    'e', 0x00,                  /* wcChar2 */
+    'r', 0x00,                  /* wcChar3 */
+    'r', 0x00,                  /* wcChar4 */
+    'y', 0x00,                  /* wcChar5 */
+    'U', 0x00,                  /* wcChar6 */
+    'S', 0x00,                  /* wcChar7 */
+    'B', 0x00,                  /* wcChar8 */
+    ' ', 0x00,                  /* wcChar9 */
+    'C', 0x00,                  /* wcChar10 */
+    'S', 0x00,                  /* wcChar11 */
+    'H', 0x00,                  /* wcChar12 */
+    ' ', 0x00,                  /* wcChar13 */
+    'D', 0x00,                  /* wcChar14 */
+    'E', 0x00,                  /* wcChar15 */
+    'M', 0x00,                  /* wcChar16 */
+    'O', 0x00,                  /* wcChar17 */
+    /*
+     * string3 descriptor
+     */
+    0x16,                       /* bLength */
+    USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
+    '2', 0x00,                  /* wcChar0 */
+    '0', 0x00,                  /* wcChar1 */
+    '2', 0x00,                  /* wcChar2 */
+    '2', 0x00,                  /* wcChar3 */
+    '1', 0x00,                  /* wcChar4 */
+    '2', 0x00,                  /* wcChar5 */
+    '3', 0x00,                  /* wcChar6 */
+    '4', 0x00,                  /* wcChar7 */
+    '5', 0x00,                  /* wcChar8 */
+    '6', 0x00,                  /* wcChar9 */
+#ifdef CONFIG_USB_HS
+    /*
+     * device qualifier descriptor
+     */
+    0x0a,
+    USB_DESCRIPTOR_TYPE_DEVICE_QUALIFIER,
+    0x00,
+    0x02,
+    0x02,
+    0x02,
+    0x01,
+    0x40,
+    0x01,
+    0x00,
+#endif
+    0x00
+};
+
+USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t usb_temp_read_buffer[CDC_MAX_MPS];
+USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t usb_temp_write_buffer[1024];
+
+static void usbd_event_handler(uint8_t busid, uint8_t event)
+{
+    switch (event) {
+    case USBD_EVENT_RESET:
+        break;
+    case USBD_EVENT_CONNECTED:
+        break;
+    case USBD_EVENT_DISCONNECTED:
+        break;
+    case USBD_EVENT_RESUME:
+        break;
+    case USBD_EVENT_SUSPEND:
+        break;
+    case USBD_EVENT_CONFIGURED:
+        /* setup first out ep read transfer */
+        usbd_ep_start_read(busid, CDC_OUT_EP, usb_temp_read_buffer, CDC_MAX_MPS);
+        break;
+    case USBD_EVENT_SET_REMOTE_WAKEUP:
+        break;
+    case USBD_EVENT_CLR_REMOTE_WAKEUP:
+        break;
+
+    default:
+        break;
+    }
+}
+
+void usbd_cdc_acm_bulk_out(uint8_t busid, uint8_t ep, uint32_t nbytes)
 {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    uint8_t irq_id = uart_get_irq_id(shell_uart);
+    chry_ringbuffer_write(&shell_rb, usb_temp_read_buffer, nbytes);
+    usbd_ep_start_read(busid, ep, usb_temp_read_buffer, CDC_MAX_MPS);
+    xEventGroupSetBitsFromISR(event_hdl, 0x10, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
 
-    if (irq_id == uart_intr_id_rx_data_avail) {
-        while (uart_check_status(shell_uart, uart_stat_data_ready)) {
-            uint8_t byte = uart_read_byte(shell_uart);
-            chry_ringbuffer_write_byte(&shell_rb, byte);
-        }
-
-        xEventGroupSetBitsFromISR(event_hdl, 0x10, &xHigherPriorityTaskWoken);
+void usbd_cdc_acm_bulk_in(uint8_t busid, uint8_t ep, uint32_t nbytes)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    if ((nbytes % CDC_MAX_MPS) == 0 && nbytes) {
+        /* send zlp */
+        usbd_ep_start_write(busid, ep, NULL, 0);
+    } else {
+        xEventGroupSetBitsFromISR(event_hdl, 0x20, &xHigherPriorityTaskWoken);
         portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
     }
 }
 
+/*!< endpoint call back */
+struct usbd_endpoint cdc_out_ep = {
+    .ep_addr = CDC_OUT_EP,
+    .ep_cb = usbd_cdc_acm_bulk_out
+};
+
+struct usbd_endpoint cdc_in_ep = {
+    .ep_addr = CDC_IN_EP,
+    .ep_cb = usbd_cdc_acm_bulk_in
+};
+
+static struct usbd_interface intf0;
+static struct usbd_interface intf1;
+
+void cdc_acm_init(uint8_t busid, uint32_t reg_base)
+{
+    usbd_desc_register(busid, cdc_descriptor);
+    usbd_add_interface(busid, usbd_cdc_acm_init_intf(busid, &intf0));
+    usbd_add_interface(busid, usbd_cdc_acm_init_intf(busid, &intf1));
+    usbd_add_endpoint(busid, &cdc_out_ep);
+    usbd_add_endpoint(busid, &cdc_in_ep);
+    usbd_initialize(busid, reg_base, usbd_event_handler);
+}
+
 static uint16_t csh_sput_cb(chry_readline_t *rl, const void *data, uint16_t size)
 {
-    uint16_t i;
     (void)rl;
-    for (i = 0; i < size; i++) {
-        if (status_success != uart_send_byte(shell_uart, ((uint8_t *)data)[i])) {
-            break;
-        }
-    }
 
-    return i;
+    memcpy(usb_temp_write_buffer, data, size);
+    usbd_ep_start_write(0, CDC_IN_EP, usb_temp_write_buffer, size);
+    xEventGroupWaitBits(event_hdl, 0x20, pdTRUE, pdFALSE, portMAX_DELAY);
+    return size;
 }
 
 static uint16_t csh_sget_cb(chry_readline_t *rl, void *data, uint16_t size)
@@ -207,16 +361,19 @@ void chry_shell_port_default_handler(chry_shell_t *csh, int sig)
     chry_readline_edit_refresh(&csh->rl);
 }
 
-int shell_init(UART_Type *uart, bool need_login)
+int shell_init(uint8_t busid, uint32_t regbase, bool need_login)
 {
     chry_shell_init_t csh_init;
 
-    if (uart == NULL) {
+    if (chry_ringbuffer_init(&shell_rb, mempool, sizeof(mempool))) {
         return -1;
     }
 
-    if (chry_ringbuffer_init(&shell_rb, mempool, sizeof(mempool))) {
-        return -1;
+    cdc_acm_init(busid, regbase);
+
+    while(!usb_device_is_configured(busid))
+    {
+        vTaskDelay(100);
     }
 
     if (need_login) {
@@ -224,8 +381,6 @@ int shell_init(UART_Type *uart, bool need_login)
     } else {
         login = true;
     }
-
-    shell_uart = uart;
 
     /*!< I/O callback */
     csh_init.sput = csh_sput_cb;
