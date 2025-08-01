d79e9302193645bd60f67b80ae8affd26aae3b02
egahp <2687434412@qq.com>
2025-08-01 13:12:18 +0800

    feat: builtin shsize add usage and help
    Signed-off-by: egahp <2687434412@qq.com>

--- builtin/shsize.c
+++ builtin/shsize.c
@@ -15,36 +15,39 @@
 static int shsize(int argc, char **argv)
 {
     chry_shell_t *csh = (void *)argv[argc + 1];
-    char *prgname = argv[0];
 
-    if ((argc == 2) && !strcmp(argv[1], "--update")) {
+    if ((argc == 2) && !strcmp(argv[1], "update")) {
         if (5 != csh->rl.sput(&csh->rl, "\e[18t", 5)) {
-            csh_printf(csh, "Failed to request update\r\n");
+            csh_printf(csh, "failed to request update\r\n");
             return -1;
         }
         return 0;
-    } else if ((argc == 4) && !strcmp(argv[1], "--config")) {
+    } else if ((argc == 4) && !strcmp(argv[1], "config")) {
         int row = atoi(argv[2]);
         int col = atoi(argv[3]);
         if (row > 10 && row < 4096) {
             csh->rl.term.row = row;
         } else {
-            csh_printf(csh, "Illegal row %d\r\n", row);
+            csh_printf(csh, "illegal row %d\r\n", row);
         }
         if (col > 10 && col < 4096) {
             csh->rl.term.col = col;
         } else {
-            csh_printf(csh, "Illegal col %d\r\n", col);
+            csh_printf(csh, "illegal col %d\r\n", col);
         }
-    } else if (argc == 1) {
-    } else {
-        csh_printf(csh, "Usage: %s [--update | --config <row> <col>\r\n", prgname);
-        return 0;
     }
 
-    csh_printf(csh, "Window config to row:%d col:%d\r\n", csh->rl.term.row, csh->rl.term.col);
+    csh_printf(csh, "window config to row:%d col:%d\r\n", csh->rl.term.row, csh->rl.term.col);
 
     return 0;
 }
 
-CSH_SCMD_EXPORT(shsize, );
+CSH_SCMD_EXPORT_FULL(
+    shsize,
+    "get or set the terminal window size",
+    "shsize\r\n"
+    "    - show current terminal window size\r\n"
+    "shsize udpate\r\n"
+    "    - request to update the window size to the terminal\r\n"
+    "shsize config <row> <col>\r\n"
+    "    - force configuration of terminal window size\r\n");
